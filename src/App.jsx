df81ccf18351ba88c9b30df7aa672a2e389ccc20
abhixhek-s <abhisheksisme4@gmail.com>
2025-08-14 20:27:00 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Data from './components/Data'
+import Apifetch from './components/Apifetch'
+
+function App() {
+
+  return (
+    <>
+    
+        <Apifetch/>
+    </>
+  )
+}
+
+export default App
